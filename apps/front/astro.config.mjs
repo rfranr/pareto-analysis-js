dabb1ee322f132a73f3d165ce3733eb990d2536c
rfranr <rfranr@gmail.com>
2025-10-19 10:07:18 +0200

    wip_ deploy

--- apps/front/astro.config.mjs
+++ apps/front/astro.config.mjs
@@ -3,6 +3,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.NODE_ENV === 'production' 
+    ? 'https://rfranr.github.io' 
+    : 'http://localhost:4321',
+  base: process.env.NODE_ENV === 'production' 
+    ? '/pareto-analysis-js' 
+    : undefined,
   vite: {
     build: {
       sourcemap: true
